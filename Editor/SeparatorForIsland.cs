25c9cf1787cb1eac0840beb1c00da2408c0c804d
Reina_Sakiria <sasiki.64892@gmail.com>
2023-10-29 03:58:05 +0900

    init !

---
+++ Editor/SeparatorForIsland.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace net.rs64.DestructiveTextureUtilities
+{
+    public class SeparatorForIsland
+    {
+
+    }
+}
