cca1f4330ff0fdc229c0f06f9bf29a731a4499e0
Reina_Sakiria <sasiki.64892@gmail.com>
2024-05-29 01:01:29 +0900

    feat: ImportedImageExtractor

---
+++ Editor/ImportedImageExtractor.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using net.rs64.TexTransTool.MultiLayerImage;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace net.rs64.DestructiveTextureUtilities
+{
+    internal class ImportedImageExtractor : DestructiveUtility
+    {
+        public TTTImportedImage TTTImportedImage;
+        public override void CreateUtilityPanel(VisualElement rootElement)
+        {
+            var serializedObject = new SerializedObject(this);
+            rootElement.hierarchy.Add(new Label("PSDなどからインポートされたレイヤーを抽出します。"));
+            rootElement.hierarchy.Add(CreateVIProperyFiled(serializedObject.FindProperty(nameof(TTTImportedImage))));
+
+            var button = new Button(Extract);
+            button.text = "Execute";
+            rootElement.hierarchy.Add(button);
+        }
+
+        void Extract()
+        {
+            var canvasBytes = File.ReadAllBytes(AssetDatabase.GetAssetPath(TTTImportedImage.CanvasDescription));
+            var imageData = TTTImportedImage.LoadImage(canvasBytes);
+
+            var tex2d = new Texture2D(TTTImportedImage.CanvasDescription.Width, TTTImportedImage.CanvasDescription.Height, TextureFormat.RGBA32, false);
+
+            tex2d.LoadRawTextureData(imageData.GetResult);
+            tex2d.name = TTTImportedImage.name + "-Extracted";
+            AssetSaveHelper.SavePNG(tex2d);
+
+            imageData.GetResult.Dispose();
+        }
+    }
+}
