593bddde9147a09e842d51a29e0689ed4cda5462
Reina_Sakiria <rs@rs64.net>
2025-04-25 14:27:13 +0900

    chore: DependentUpdater

---
+++ DependentUpdater~/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Write dependency version start");
+
+Directory.SetCurrentDirectory("../"); // move to repository root
+const string DEPENDENCY_PACKAGE_DOT_JSON = "../TexTransTool/package.json";
+var thisPackageJsonPath = @"package.json";
+
+
+var dependencyPackageJson = System.Text.Json.Nodes.JsonNode.Parse(File.ReadAllText(DEPENDENCY_PACKAGE_DOT_JSON));
+if (dependencyPackageJson is null) { throw new NullReferenceException(); }
+var dependencyVersion = dependencyPackageJson["version"]!.GetValue<string>();
+var dependencyCode = dependencyPackageJson["name"]!.GetValue<string>();
+
+
+var thisPackageJson = System.Text.Json.Nodes.JsonNode.Parse(File.ReadAllText(thisPackageJsonPath));
+if (thisPackageJson is null) { throw new NullReferenceException(); }
+
+thisPackageJson["dependencies"]![dependencyCode] = dependencyVersion;
+thisPackageJson["vpmDependencies"]![dependencyCode] = "^" + dependencyVersion;
+
+var outOpt = new System.Text.Json.JsonSerializerOptions(System.Text.Json.JsonSerializerDefaults.General) { WriteIndented = true };
+File.WriteAllText(thisPackageJsonPath, thisPackageJson.ToJsonString(outOpt) + "\n");
+Console.WriteLine("Write version exit!");
